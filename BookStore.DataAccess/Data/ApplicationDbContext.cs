fa2518bf721f8c51b3d33315520f0ca257207230
Danial <Danialsari13@gmail.com>
2024-01-13 10:07:13 +0330

    add Home UI

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using System.ComponentModel;
 
 namespace BookStore.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : identityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
