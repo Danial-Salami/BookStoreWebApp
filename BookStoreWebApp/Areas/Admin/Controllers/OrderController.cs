988aefb6b6f31890a67c407dbc8d67ded64999b5
Danial <Danialsari13@gmail.com>
2024-01-28 16:31:31 +0330

    add orderVM and orderController

---
+++ BookStoreWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebApp.Areas.Admin.Controllers
+{
+	public class OrderController(IUnitOfWork unitOfWork) : Controller
+	{
+
+		private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		#region API CALL
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader
+				.GetAll(includeProperties: "ApplicationUser")
+				.ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+	
+		#endregion
+	}
+}
---
+++ BookStoreWebApp/wwwroot/js/order.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { "url" :'/admin/product/getall' },
+        "columns": [
+            { data: 'id',"width": "10%" },
+            { data: 'name', "width": "10%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'applicationuser.email', "width": "15%" },
+            { data: 'orderstatus', "width": "10%" },
+            { data: 'ordertotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-100 btn-group " role="group"><a href="/admin/product/upsert?id=${data}" class="btn btn-info mx-2 p-1" ><i class="bi bi-pencil-square"></i> Edit</a ></div>`;
+                },
+                "width": "25%"
+            },
+
+        ],
+        "initComplete": function (settings, json) {
+            console.log("DataTables has been initialized with the following data:", json);
+        },
+        "error": function (xhr, error, thrown) {
+            console.error("Error loading DataTable:", error);
+        }
+    });               
+}
